3dc34b5024a1763807a9cfe07f28ed7e3252549a
Luka <lukaneek@gmail.com>
2025-02-18 11:52:17 -0600

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { Link } from "react-router-dom";
 function App() {
   
   return (
     <>
       <h1>Luka Vujasin Homepage</h1>
-      <Link to={"/example"}>example</Link>
+      <div>
+
+      <a href="/example">Link To Another Application</a>
+      <br/>
+      <a target="_blank" href="https://github.com/lukaneek/lukavujasinxyz">Link To This Applications Repository</a>
+      </div>
     </>
   )
 }
