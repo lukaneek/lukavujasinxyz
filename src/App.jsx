e66753e91fa545159471abc40b6432d944a8643d
Luka <lukaneek@gmail.com>
2025-03-05 14:59:52 -0600

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AboutOtherApps from "./components/AboutOtherApps";
 function App() {
 
   return (
-    <div style={{paddingTop:50, paddingBottom: 50}} class="container flex align-items-center">
+    <div style={{paddingTop: 50, paddingBottom: 50}} class="container flex align-items-center">
 
       <div class="d-flex align-items-center">
           <img style={{borderRadius: 100}} src="/avatar.jpg" alt="lukas profile picture" height="120" width="100" />
