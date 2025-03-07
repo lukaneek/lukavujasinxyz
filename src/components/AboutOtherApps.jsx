77f45fd2814e3192614c0fc94060377ee060320e
lukaneek <86979747+lukaneek@users.noreply.github.com>
2025-03-07 06:39:57 -0600

    Update AboutOtherApps.jsx

--- src/components/AboutOtherApps.jsx
+++ src/components/AboutOtherApps.jsx
@@ -3,9 +3,9 @@ const AboutOtherApps = () => {
     return (
         <>
             <h3>About other apps</h3>
-            <p>The following links are other working apps that I have created using different technologies.  Each link will describe the app and a link to its repository.</p>
+            <p>The following links are other working apps that I have created using different technologies.  Each link will take you to the app and its repository.</p>
             <p/>
-            <a href="/example/">Pizza site built with react, express, and mongoDB</a>
+            <a href="/example/">In progress, React frontend and Express backend</a>
         </>
     );
 }
