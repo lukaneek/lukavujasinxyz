5f2f10f1270d9629c494d04c199114c78c5566cc
lukaneek <86979747+lukaneek@users.noreply.github.com>
2025-03-07 12:54:47 -0600

    Update AboutOtherApps.jsx

--- src/components/AboutOtherApps.jsx
+++ src/components/AboutOtherApps.jsx
@@ -5,7 +5,7 @@ const AboutOtherApps = () => {
             <h3>About other apps</h3>
             <p>The following links are other working apps that I have created using different technologies.  Each link will take you to the app and its repository.</p>
             <p/>
-            <a href="/example/">In progress, React frontend and Express backend</a>
+            <a href="/example/">React frontend and Express backend running on 2 ec2 instances</a>
         </>
     );
 }
