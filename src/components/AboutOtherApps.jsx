b08c4f14c6c98edda715f0c2761bee9eb78b2839
Luka <lukaneek@gmail.com>
2025-04-02 09:09:09 -0500

    update

--- src/components/AboutOtherApps.jsx
+++ src/components/AboutOtherApps.jsx
@@ -5,13 +5,13 @@ const AboutOtherApps = () => {
             <h3>About other apps</h3>
             <p>The following links are other working apps that I have created using different technologies.  Each link will take you to the app and its repository.</p>
             <p/>
-            <a href="/example/">React frontend and Express backend running on 2 EC2 instances</a>
+            <a href="#">COMING SOON... Flight search site written in React and Java</a>
             <p/>
-            <a href="/comics/">Comic book store written in Java and JSP</a>
+            <a href="/users/">Simple CRUD app written in React and Java using Google address verification API's</a>
             <p/>
-            <a href="/pizzas/">Pizza store written in JavaScript, React and Express</a>
+            <a href="/comics/">Comic book store written in Java with JSP's</a>
             <p/>
-            <a href="/users/">Simple CRUD app written in React, Java and Google address verification API's</a>
+            <a href="/pizzas/">Pizza store written in Javascript, React and Express</a>
         </>
     );
 }
